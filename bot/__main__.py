86f45917e38e4cbd5dd6b1e608c8790f086257bb
rhliang <rliang@cfenet.ubc.ca>
2018-07-26 22:40:04 -0700

    Changed __main__.py so that this bot *only* spams.

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,20 +5,21 @@ import json
 
 from discord.ext import commands
 
-from bot.verification_cog import VerificationCog
-from bot.verification_db import VerificationDB
-from bot.guild_logging_cog import GuildLoggingCog
-from bot.guild_logging_db import GuildLoggingDB
-from bot.ex_gate_cog import EXGateCog
-from bot.ex_gate_db import EXGateDB
-from bot.role_reaction_subscription_cog import RoleReactionSubscriptionCog
-from bot.role_reaction_subscription_db import RoleReactionSubscriptionDB
-from bot.baconpatroll_cog import BaconpaTrollCog
-from bot.no_command_subscription_cog import NoCommandSubscriptionCog
-from bot.no_command_subscription_db import NoCommandSubscriptionDB
-from bot.role_set_operations_cog import RoleSetOperationsCog
-# from bot.spam_cog import SpamCog
-from bot.purge_channels_cog import PurgeChannelsCog
+# from bot.verification_cog import VerificationCog
+# from bot.verification_db import VerificationDB
+# from bot.guild_logging_cog import GuildLoggingCog
+# from bot.guild_logging_db import GuildLoggingDB
+# from bot.ex_gate_cog import EXGateCog
+# from bot.ex_gate_db import EXGateDB
+# from bot.role_reaction_subscription_cog import RoleReactionSubscriptionCog
+# from bot.role_reaction_subscription_db import RoleReactionSubscriptionDB
+# from bot.baconpatroll_cog import BaconpaTrollCog
+# from bot.no_command_subscription_cog import NoCommandSubscriptionCog
+# from bot.no_command_subscription_db import NoCommandSubscriptionDB
+# from bot.role_set_operations_cog import RoleSetOperationsCog
+# from bot.purge_channels_cog import PurgeChannelsCog
+
+from bot.spam_cog import SpamCog
 
 __author__ = "Richard Liang"
 
@@ -35,26 +36,26 @@ def main():
 
     gvrd_grunt = commands.Bot(
         command_prefix=commands.when_mentioned_or(settings["command_prefix"]),
-        description="A grunt worker for the GVRD servers' needs"
+        description="Spams a channel."
     )
-    verification_db = VerificationDB(settings["sqlite_db"])
-    logging_db = GuildLoggingDB(settings["sqlite_db"])
-    ex_db = EXGateDB(settings["sqlite_db"])
-    role_reaction_subscription_db = RoleReactionSubscriptionDB(settings["sqlite_db"])
-    no_command_subscription_db = NoCommandSubscriptionDB(settings["sqlite_db"])
+    # verification_db = VerificationDB(settings["sqlite_db"])
+    # logging_db = GuildLoggingDB(settings["sqlite_db"])
+    # ex_db = EXGateDB(settings["sqlite_db"])
+    # role_reaction_subscription_db = RoleReactionSubscriptionDB(settings["sqlite_db"])
+    # no_command_subscription_db = NoCommandSubscriptionDB(settings["sqlite_db"])
 
-    logging_cog = GuildLoggingCog(gvrd_grunt, logging_db)
-    gvrd_grunt.add_cog(VerificationCog(gvrd_grunt, verification_db))
-    gvrd_grunt.add_cog(logging_cog)
-    gvrd_grunt.add_cog(EXGateCog(gvrd_grunt, ex_db, logging_cog=logging_cog))
-    gvrd_grunt.add_cog(RoleReactionSubscriptionCog(gvrd_grunt, role_reaction_subscription_db, logging_cog=logging_cog))
-    gvrd_grunt.add_cog(BaconpaTrollCog(gvrd_grunt))
-    gvrd_grunt.add_cog(NoCommandSubscriptionCog(gvrd_grunt, no_command_subscription_db, logging_cog=logging_cog))
-    gvrd_grunt.add_cog(RoleSetOperationsCog(gvrd_grunt))
-    gvrd_grunt.add_cog(PurgeChannelsCog(gvrd_grunt))
+    # logging_cog = GuildLoggingCog(gvrd_grunt, logging_db)
+    # gvrd_grunt.add_cog(VerificationCog(gvrd_grunt, verification_db))
+    # gvrd_grunt.add_cog(logging_cog)
+    # gvrd_grunt.add_cog(EXGateCog(gvrd_grunt, ex_db, logging_cog=logging_cog))
+    # gvrd_grunt.add_cog(RoleReactionSubscriptionCog(gvrd_grunt, role_reaction_subscription_db, logging_cog=logging_cog))
+    # gvrd_grunt.add_cog(BaconpaTrollCog(gvrd_grunt))
+    # gvrd_grunt.add_cog(NoCommandSubscriptionCog(gvrd_grunt, no_command_subscription_db, logging_cog=logging_cog))
+    # gvrd_grunt.add_cog(RoleSetOperationsCog(gvrd_grunt))
+    # gvrd_grunt.add_cog(PurgeChannelsCog(gvrd_grunt))
 
     # For testing only -- *do not install on a production bot!*
-    # gvrd_grunt.add_cog(SpamCog())
+    gvrd_grunt.add_cog(SpamCog())
 
     @gvrd_grunt.event
     async def on_command_error(ctx, error):
