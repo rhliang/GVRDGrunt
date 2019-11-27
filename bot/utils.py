23c1a5ba59352c0dcd97ac0d6767371bed5d11f8
rhliang <rliang@cfenet.ubc.ca>
2019-11-26 22:55:31 -0800

    Added a "pagination" for long message texts, as progress toward #43.

--- bot/utils.py
+++ bot/utils.py
@@ -13,3 +13,21 @@ def emoji_to_db(emoji):
         emoji_type = "custom"
         emoji_stored_value = emoji.id
     return emoji_type, emoji_stored_value
+
+
+def break_up_long_message(message_text):
+    """
+    Given a message text, break it into chunks of < 2000 characters.
+    :param message_text:
+    :return:
+    """
+    chunks = []
+    curr_message = ""
+    for line in message_text.splitlines(keepends=True):
+        if len(curr_message) + len(line) > 2000:
+            chunks.append(curr_message)
+            curr_message = line
+        else:
+            curr_message += line
+    chunks.append(curr_message)
+    return chunks
