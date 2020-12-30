c7e218d6e4c02198dee7f43bc55592ae54d72bfa
Richard Liang <rliang@bccfe.ca>
2020-12-30 00:02:00 -0800

    Fixed an issue with the migration script not correctly handling non-custom emoji.

--- db_migration_scripts/verification_migration.py
+++ db_migration_scripts/verification_migration.py
@@ -25,15 +25,21 @@ def get_all_verification_configuration(conn, new_table_name="GuildVerification")
                 "instinct_role": {"N": str(row[5])},
                 "mystic_role": {"N": str(row[6])},
                 "valor_role": {"N": str(row[7])},
-                "instinct_emoji": {"N": str(row[8])},
+                "instinct_emoji": row[8],  # this is post-processed
                 "instinct_emoji_type": {"S": row[9]},
-                "mystic_emoji": {"N": str(row[10])},
+                "mystic_emoji": row[10],  # this is post-processed
                 "mystic_emoji_type": {"S": row[11]},
-                "valor_emoji": {"N": str(row[12])},
+                "valor_emoji": row[12],  # this is post-processed
                 "valor_emoji_type": {"S": row[13]},
                 "welcome_message": {"S": row[14]},
                 "welcome_channel": {"N": str(row[15])}
             }
+
+            for team in ("instinct", "mystic", "valor"):
+                if guild_config[f"{team}_emoji_type"]["S"] == "custom":
+                    guild_config[f"{team}_emoji"] = {"N": str(guild_config[f"{team}_emoji"])}
+                else:
+                    guild_config[f"{team}_emoji"] = {"S": guild_config[f"{team}_emoji"]}
 
             # Retrieve all standard roles for this guild.
             channel_mapping_cursor = conn.execute(
